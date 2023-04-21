1c7c9e58065b77d308437cf05db4d31c865f504b
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-04-21 21:34:21 +0300

    Liskov substitution Principle-1

---
+++ 2-Solid/Solid.App/LiskovSubstition/LSPBad.cs
@@ -0,0 +1,29 @@
+﻿namespace Solid.App.LiskovSubstition.Bad
+{
+    public abstract class BasePhone
+    {
+        public void Call()
+        {
+            Console.WriteLine("Arama Yapıldı");
+        }
+
+        public abstract void TakePhoto();
+    }
+
+    public class IPhone : BasePhone
+    {
+        public override void TakePhoto()
+        {
+            Console.WriteLine("Fotoğraf Çekildi");
+        }
+    }
+
+    public class Nokia3310 : BasePhone
+    {
+        public override void TakePhoto()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+}
--- 2-Solid/Solid.App/Program.cs
+++ 2-Solid/Solid.App/Program.cs
@@ -1,10 +1,12 @@
-﻿
+﻿#region OpenClosed Principle
+
 using Solid.App.OpenClosed.Bad;
 using p = Solid.App.OpenClosed.Good;
 using Solid.App.OpenClosed.BestPractice;
+using Solid.App.LiskovSubstition.Bad;
 
 //Solid.App.OpenClosed.Good.SalaryCalculator salaryCalculatorGood = new();
-Solid.App.OpenClosed.BestPractice.SalaryCalculator salaryCalculatorBestPractice = new();
+//Solid.App.OpenClosed.BestPractice.SalaryCalculator salaryCalculatorBestPractice = new();
 
 //Bad Way
 //Console.WriteLine($"Low Salary : { salaryCalculator.Calculate(1000, SalaryType.Low)}");
@@ -18,12 +20,41 @@ Solid.App.OpenClosed.BestPractice.SalaryCalculator salaryCalculatorBestPractice 
 //Console.WriteLine($"Manager Salary : {salaryCalculatorGood.Calculate(1000, new ManagerSalaryCalculate())}");
 
 //Best Practice Way
-Console.WriteLine($"Low Salary : {salaryCalculatorBestPractice.Calculate(1000, new LowSalaryCalculate().Calculate)}");
-Console.WriteLine($"Medium Salary : {salaryCalculatorBestPractice.Calculate(1000, new MediumSalaryCalculate().Calculate)}");
-Console.WriteLine($"High Salary : {salaryCalculatorBestPractice.Calculate(1000, new HighSalaryCalculate().Calculate)}");
-Console.WriteLine($"Manager Salary : {salaryCalculatorBestPractice.Calculate(1000, new ManagerSalaryCalculate().Calculate)}");
+//Console.WriteLine($"Low Salary : {salaryCalculatorBestPractice.Calculate(1000, new LowSalaryCalculate().Calculate)}");
+//Console.WriteLine($"Medium Salary : {salaryCalculatorBestPractice.Calculate(1000, new MediumSalaryCalculate().Calculate)}");
+//Console.WriteLine($"High Salary : {salaryCalculatorBestPractice.Calculate(1000, new HighSalaryCalculate().Calculate)}");
+//Console.WriteLine($"Manager Salary : {salaryCalculatorBestPractice.Calculate(1000, new ManagerSalaryCalculate().Calculate)}");
 
-Console.WriteLine($"Custom Salary : {salaryCalculatorBestPractice.Calculate(1000, x =>
+//Console.WriteLine($"Custom Salary : {salaryCalculatorBestPractice.Calculate(1000, x =>
+//{
+//    return x * 10;
+//})}");
+#endregion
+#region Liskov Substition Principle
+//BasePhone phone = new Iphone();
+
+//phone.Call();
+//phone.TakePhoto();
+
+//phone = new Nokia3310();
+
+//phone.Call();
+//phone.TakePhoto();
+BasePhone phone;
+int i = 1;
+
+if (i == 1)
 {
-    return x * 10;
-})}");
+    phone = new IPhone();
+    phone.Call();
+    phone.TakePhoto();
+}
+else
+{
+    phone = new Nokia3310();
+    phone.Call();
+    phone.TakePhoto();
+}
+
+
+#endregion
