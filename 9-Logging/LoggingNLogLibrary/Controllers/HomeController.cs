efcb777d6f3192bd082d722cd76958f7a087b6e3
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-05-14 13:12:08 +0300

    NLog kütüphanesinin projeye dahil edilmesi

---
+++ 9-Logging/LoggingNLogLibrary/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using LoggingNLogLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LoggingNLogLibrary.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Index sayfası başlamıştır..");
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ 9-Logging/LoggingNLogLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LoggingNLogLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
