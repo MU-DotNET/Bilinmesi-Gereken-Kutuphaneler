dbe7d01bdb7e0e722405869ca489be170f5df70f
Musa UYUMAZ <113431738+musauyumaz@users.noreply.github.com>
2023-05-14 20:49:40 +0300

    Swagger ile ilgili servis ve middleware ekleme

--- 10-Swagger/SwaggerApp/Program.cs
+++ 10-Swagger/SwaggerApp/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using SwaggerApp.Models;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,27 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(gen =>
+{
+    gen.SwaggerDoc("v1", new()
+    {
+        Version = "V1",
+        Title = "Product API",
+        Description = "Ürün Ekleme/Silme/Güncelleme iþlemlerini gerçekleþtiren api",
+        Contact = new()
+        {
+            Name = "Musa UYUMAZ",
+            Email = "musa.uyumaz73@gmail.com",
+            Url = new("www.musauyumaz.com")
+        }
+    });
+
+    string xmlFile = $"{Assembly.GetExecutingAssembly().GetName()}.xml";
+    string xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+
+    gen.IncludeXmlComments(xmlPath);
+});
+
 
 builder.Services.AddDbContext<SwaggerDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MsSQL")));
 
@@ -18,7 +40,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API");
+    });
 }
 
 app.UseHttpsRedirection();
